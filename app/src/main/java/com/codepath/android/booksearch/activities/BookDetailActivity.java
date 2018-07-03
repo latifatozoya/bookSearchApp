35c6978ea10f71565156abecba9607028c058ed5
Luis Iván Morett Arévalo <ivanmorett@fb.com>
2018-07-03 13:19:26 -0700

    first commit

---
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -0,0 +1,197 @@
+package com.codepath.android.booksearch.activities;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.codepath.android.booksearch.GlideApp;
+import com.codepath.android.booksearch.R;
+import com.codepath.android.booksearch.models.Book;
+
+import org.parceler.Parcels;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BookDetailActivity extends AppCompatActivity {
+    private ImageView ivBookCover;
+    private TextView tvTitle;
+    private TextView tvAuthor;
+    private TextView tvPublishDate, tvPublisher;
+    private Intent shareIntent;
+
+    private ShareActionProvider miShareAction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_detail);
+
+        Toolbar toolbar = findViewById(R.id.detail_toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+        // Fetch views
+        ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvAuthor = (TextView) findViewById(R.id.tvAuthor);
+        tvPublisher = findViewById(R.id.tvPublisher);
+        tvPublishDate = findViewById(R.id.tvPublishDate);
+
+        // Extract book object from intent extras
+        Book book =  (Book) Parcels.unwrap(getIntent().getParcelableExtra("book"));
+        // Use book object to populate data into views
+        tvTitle.setText(book.getTitle());
+        tvAuthor.setText(book.getAuthor());
+        tvPublishDate.setText(book.getPublish_date());
+        tvPublisher.setText(book.getPublisher());
+
+        GlideApp.with(getApplicationContext())
+                .load(Uri.parse(book.getCoverUrl()))
+                .placeholder(R.drawable.ic_nocover)
+//                .listener(new RequestListener<Drawable>() {
+//
+//                    @Override
+//                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+//
+//                        return false;
+//                    }
+//
+//                    @Override
+//                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+//                        prepareShareIntent(((BitmapDrawable) resource).getBitmap());
+//                        attachShareIntentAction();
+//
+//                        return false;
+//                    }
+//
+//                 })
+                .into(ivBookCover);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_book_detail, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+//    public void prepareShareIntent(Bitmap drawableImage) {
+//
+//        // Fetch Bitmap Uri locally
+//
+//        Uri bmpUri = getBitmapFromDrawable(drawableImage);// see previous remote images section and notes for API > 23
+//
+//        // Construct share intent as described above based on bitmap
+//
+//        shareIntent = new Intent();
+//
+//        shareIntent.setAction(Intent.ACTION_SEND);
+//
+//        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+//
+//        shareIntent.setType("image/*");
+//
+//    }
+//
+//
+//
+//// Attaches the share intent to the share menu item provider
+//
+//    public void attachShareIntentAction() {
+//
+//        if (miShareAction != null && shareIntent != null)
+//
+//            miShareAction.setShareIntent(shareIntent);
+//
+//    }
+//
+//    // Method when launching drawable within Glide
+//
+//    public Uri getBitmapFromDrawable(Bitmap bmp){
+//
+//
+//
+//        // Store image to default external storage directory
+//
+//        Uri bmpUri = null;
+//
+//        try {
+//
+//            // Use methods on Context to access package-specific directories on external storage.
+//
+//            // This way, you don't need to request external read/write permission.
+//
+//            // See https://youtu.be/5xVh-7ywKpE?t=25m25s
+//
+//            File file =  new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+//
+//            FileOutputStream out = new FileOutputStream(file);
+//
+//            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+//
+//            out.close();
+//
+//
+//
+//            // wrap File object into a content provider. NOTE: authority here should match authority in manifest declaration
+//
+//            bmpUri = FileProvider.getUriForFile(BookDetailActivity.this, "com.codepath.fileprovider", file);  // use this version for API >= 24
+//
+//
+//
+//            // **Note:** For API < 24, you may use bmpUri = Uri.fromFile(file);
+//
+//
+//
+//        } catch (IOException e) {
+//
+//            e.printStackTrace();
+//
+//        }
+//
+//        return bmpUri;
+//
+//    }
+}
